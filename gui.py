9157e60c02e309f123a319f95084b76d103de8f4
Gavin Lobo <gavinl@patriot1tech.com>
2018-06-12 10:44:13 -0400

    Added comments to gui.py

--- gui.py
+++ gui.py
@@ -5,35 +5,37 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 
 from MyWidget import MyWidget
-from widget1 import Widget1
 
 # Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
 
     app = QtGui.QApplication([])
 
-
+    # Create a main window
     main_window = QtGui.QMainWindow()
     main_window.resize(1024, 768)
+
+    # Create the central widget which will store all child widgets
     central_widget = QtGui.QWidget()
-    central_widget.setLayout(QtGui.QVBoxLayout())
+    central_widget.setLayout(QtGui.QVBoxLayout()) # We want to use a VBoxLayout
     main_window.setCentralWidget(central_widget)
 
+    # show the main window
+    main_window.show()
+
+    # Add 2 widgets to the main window.
+    myWidget1 = MyWidget()
+    myWidget2 = MyWidget()
+    central_widget.layout().addWidget(myWidget1)
+    central_widget.layout().addWidget(myWidget2)
 
     main_window.show()
 
-    w = Widget1()
-    central_widget.layout().addWidget( w )
-    w = Widget1()
-    central_widget.layout().addWidget( w )
 
-    #myWidget1 = MyWidget()
-    #myWidget2 = MyWidget()
+    # Create a new widget by itself as a new window
+    myWidget3 = MyWidget()
+    myWidget3.show()
 
-    #central_widget.layout().addWidget(myWidget1)
-    #central_widget.layout().addWidget(myWidget2)
-    #myWidget2.show()
-    #main_window.show()
 
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
